4d2d34e3c9e4006e5f75d289cd5e8ff525e1a0c0
Chetan Chauhan <devilmafia71@gmail.com>
2025-06-17 15:13:19 +0530

    render table to main page

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState([
+    {
+      id: "1",
+      name: "Chetan",
+      age: "22",
+      postion: "student",
+      status: false,
+    },
+    {
+      id: "2",
+      name: "Mohit",
+      age: "22",
+      postion: "Teacher",
+      status: false,
+    },
+    {
+      id: "3",
+      name: "Chetan",
+      age: "22",
+      postion: "student",
+      status: false,
+    },
+    {
+      id: "4",
+      name: "Chetan",
+      age: "22",
+      postion: "student",
+      status: false,
+    },
+  ]);
+
+  const keys = Object.keys(data[0]);
+
+  console.log(keys);
+
+  return (
+    <>
+      <div className="parent">
+        {keys.map((title) => (
+          <div className="column">{title}</div>
+        ))}
+      </div>
+
+      
+        {data.map((obj) => {
+          return (
+            <div className="parent"> 
+              {keys.map((key1, i) => {
+                return (
+                  <div className="column" key={i}>
+                    {obj[key1]}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+    
+    </>
+  );
+}
+
+export default App;
